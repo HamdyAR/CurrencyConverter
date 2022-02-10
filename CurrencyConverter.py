d1a01ab3ffd03db61f9d794682546b2faa5a9d88
Hamdalah Aremu <aremu.hamdalah@gmail.com>
2022-02-10 19:41:07 +0100

    added the Procfile and Streamlit Setup

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,29 +1,40 @@
 import streamlit as st
 from PIL import Image
-img = Image.open("calculator-image.jpg")
-st.image(img, width=600)
-st.title("Currency Converter")
-st.header("This app is designed to convert naira to US dollars, pounds and euros.")
+img = Image.open("currency.jpg")
+st.image(img, width=700)
+st.title("Naira Converter")
+st.header("A simple currency converter that converts naira only to US dollars, pounds and euros")
+st.subheader("Exchange rates as of 10th February, 2022")
+st.subheader("1 USD = 416.87 naira")
+st.subheader("1 GBP = 568.10 naira")
+st.subheader("1 EUR = 478.59 naira")
 amount = st.number_input("Enter your amount in naira")
-desiredCurrency = st.radio("Select the currency you want to convert naira to: ", ("dollars", "pounds", "euros"))
+desiredCurrency = st.radio("Select the currency that you want to convert naira to: ", ("dollars", "pounds", "euros"))
+dollarSymbol = "$"
+poundSymbol = "£"
+euroSymbol = "€"
+currencySymbols = ["$", "£", "€"] 
 if (desiredCurrency == "dollars"):
     try:
-        convertedCurrency = amount / 416.20
+        convertedCurrency = amount / 416.87
+        currencySymbol = currencySymbols[0]
     except:
         st.text('Please select your desired currency')
 elif (desiredCurrency == "pounds"):
     try:
-        convertedCurrency = amount / 563.44
+        convertedCurrency = amount / 568.19
+        currencySymbol = currencySymbols[1]
     except:
         st.text('Please select your desired currency')
 else:
     try:
-        convertedCurrency = amount / 476.51
+        convertedCurrency = amount / 478.59
+        currencySymbol = currencySymbols[2]
     except:
          st.text('Please select your desired currency')
-            
+convertedCurrency =  round(convertedCurrency, 2)             
 if(st.button("Convert your naira")):
-    st.text('Your naira equivalent in {} is {}.' .format(desiredCurrency, convertedCurrency))
-    st.success("You have successfully converted your naira")
+    st.text('The naira equivalent of ₦{} is {}{}.' .format(amount, currencySymbol, convertedCurrency))
+    st.success("You have successfully converted your naira!")
         
         
